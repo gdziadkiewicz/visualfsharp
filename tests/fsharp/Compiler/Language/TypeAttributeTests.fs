2ec37345a95c212960bddb116c2929239aa8c422
gdziadkiewicz <grzegorzdziadkiewicz@gmail.com>
2020-01-11 21:03:12 +0100

    WIP

--- tests/fsharp/Compiler/Language/TypeAttributeTests.fs
+++ tests/fsharp/Compiler/Language/TypeAttributeTests.fs
@@ -14,6 +14,77 @@ module TypeAttributeTests =
 type Point = {x:int; y:int}
             """
     [<Test>]
+    let ``Attribute can be applied to type definition 2``() = 
+        CompilerAssert.Pass
+            """
+type [<Struct>] Point = {x:int; y:int}
+            """
+    [<Test>]
+    let ``Attribute can be applied to type definition 3``() = 
+        CompilerAssert.Pass
+            """
+[<Struct>]
+type [<Struct>] Point = {x:int; y:int}
+            """
+    [<Test>]
+    let ``Attribute can be applied to type definition 4``() = 
+        CompilerAssert.Pass
+            """
+[<Struct>]
+type [<NoEquality;NoComparison>] Point = {x:int; y:int}
+            """
+    
+    [<Test>]
+    let ``Attribute can be applied to module 1``() = 
+        CompilerAssert.Pass
+            """
+[<AutoOpen>]
+module m =
+    let x = 2
+            """
+
+    [<Test>]
+    let ``Attribute can be applied to module 2``() = 
+        CompilerAssert.Pass
+            """
+module [<AutoOpen>] m =
+    let x = 2
+        """
+    
+    [<Test>]
+    let ``Attribute can be applied to module 3``() = 
+        CompilerAssert.Pass
+            """
+[<AutoOpen;AutoOpen>]
+module m
+let x = 2
+"""
+
+    [<Test>]
+    let ``Attribute can be applied to module 4``() = 
+        CompilerAssert.Pass
+            """
+module [<AutoOpen>] m
+let x = 2
+"""
+    [<Test>]
+    let ``Attribute can be applied to module 5``() = 
+        CompilerAssert.Pass
+            """
+[<AutoOpen>]
+module [<AutoOpen>] m
+let x = 2
+"""
+    [<Test>]
+    let ``Attribute can be applied to module 6``() = 
+        CompilerAssert.Pass
+            """
+[<AutoOpen>]
+module [<AutoOpen>] m =
+    let x = 2
+"""
+
+    [<Test>]
     let ``Attribute cannot be applied to optional type extension``() = 
         CompilerAssert.TypeCheckSingleError
             """
